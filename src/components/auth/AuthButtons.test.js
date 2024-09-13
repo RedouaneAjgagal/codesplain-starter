38b4d0e91f98ff8cc0f47258028ec3afb0fb2f32
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-09-13 01:54:11 +0100

    [TEST] when the user is signed it - clear cache

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -2,17 +2,20 @@ import { render, screen } from "@testing-library/react";
 import AuthButtons from "./AuthButtons";
 import createServer from "../../test/server";
 import { MemoryRouter } from "react-router";
+import { SWRConfig } from "swr";
 
 const renderComponent = async () => {
     render(
-        <MemoryRouter>
-            <AuthButtons />
-        </MemoryRouter>
+        <SWRConfig value={{ provider: () => new Map() }}>
+            <MemoryRouter>
+                <AuthButtons />
+            </MemoryRouter>
+        </SWRConfig>
     );
     await screen.findAllByRole("link");
 }
 
-describe.only("when the user is not signed in", () => {
+describe("when the user is not signed in", () => {
     createServer([
         {
             method: "get",
@@ -25,7 +28,7 @@ describe.only("when the user is not signed in", () => {
         }
     ]);
 
-    test('should display sign in and sign up', async () => {
+    test('should display sign in and sign up links', async () => {
         await renderComponent();
 
         const signIn = screen.getByRole("link", {
@@ -39,7 +42,7 @@ describe.only("when the user is not signed in", () => {
         expect(signUp).toBeInTheDocument();
     });
 
-    test('should not display sign out', async () => {
+    test('should not display sign out link', async () => {
         await renderComponent();
 
         const signOut = screen.queryByRole("link", {
@@ -48,5 +51,45 @@ describe.only("when the user is not signed in", () => {
 
         expect(signOut).not.toBeInTheDocument();
     });
+});
 
+describe("when the user is signed in", () => {
+    createServer([
+        {
+            method: "get",
+            path: "/api/user",
+            res: () => {
+                return {
+                    user: {
+                        id: 1,
+                        email: "red@red.com"
+                    }
+                }
+            }
+        }
+    ]);
+
+    test('should display sign out link', async () => {
+        await renderComponent();
+
+        const singOutLink = screen.getByRole("link", {
+            name: /sign out/i
+        });
+
+        expect(singOutLink).toBeInTheDocument();
+    });
+
+    test('should not display sign in and sign up links', async () => {
+        await renderComponent();
+
+        const signInLink = screen.queryByRole("link", {
+            name: /sign in/i
+        });
+        const signUpLink = screen.queryByRole("link", {
+            name: /sign up/i
+        });
+
+        expect(signInLink).not.toBeInTheDocument();
+        expect(signUpLink).not.toBeInTheDocument();
+    });
 });
